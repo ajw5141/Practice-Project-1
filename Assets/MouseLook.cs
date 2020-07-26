9b57d025581b95835fd84e6626fd5b4819886e1d
ajw5141 <57472854+ajw5141@users.noreply.github.com>
2020-07-26 14:46:32 -0400

    Initial commit

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //Mouse Look Variables
+    public float mouseSensitivityX = 300f;
+    public float mouseSensitivityY = 200f;
+    public Transform playerBody;
+    float xRotation = 0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ForMouse Look
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Mouse Look Controls
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        #endregion
+
+
+
+
+
+        //Things to Consider:
+        //create menu options to adjust sensitivity
+
+        //Questions:
+        //Why do my sens defaults in the inspector not match what I coded?
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -2 * 9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+
+        // Questions:
+        // Why do you need Time.deltaTime for Move but not for the x and z variables?
+        // why does my player hop up off the ground and stay there?
+
+        // Ideas:
+        // Make an interesting variable form of variable for the new planet!
+
+    }
+}
